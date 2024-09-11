f19e7d756b95c38ae0e02d704c347646846d6993
SuhaShahid <suhashahid.123@gmail.com>
2024-09-11 22:50:30 +0500

    veg shop

---
+++ category/category.js
@@ -0,0 +1,31 @@
+let categoriesData = [];
+
+function getData() {
+  fetch("https://dummyjson.com/products/categories")
+    .then((res) => res.json())
+    .then((data) => {
+      categoriesData = data; 
+      console.log(categoriesData);
+      getPage(categoriesData);
+    })
+    .catch((error) => console.error("Error:", error));
+}
+
+function getPage(data) {
+  let boxes = document.querySelector(".boxes");
+  let categoryBoxes = "";
+
+  for (let i = 0; i < data.length; i++) {
+    let category = data[i];
+    categoryBoxes += `
+      <div class="box">
+          <h3>${category.name}</h3>
+        <button type="button">Buy</button>
+      </div>
+    `;
+  }
+
+  boxes.innerHTML = categoryBoxes;
+}
+
+getData();
